75329ed860e056d725a51f3ca8939e10fc151d63
Vlad Ivanov <vivanov@metaprime.ru>
2020-04-01 14:26:16 +0300

    prod

--- module.py
+++ module.py
@@ -1,23 +1,14 @@
 import os
 
+
 class PWM:
     def __init__(self):
         pass
 
-    def set(self, values):
+    @staticmethod
+    def set(values):
         cmd = 'echo \"'
         for pin in values:
             cmd += str(pin) + '=' + str(values[pin]) + ';'
-
         cmd += '\" > /dev/pi-blaster'
-        print(cmd)
-        # os.system(cmd)
-
-
-
-a = PWM()
-
-a.set({
-    1: 2.46,
-    16: 0.3455
-})
+        os.system(cmd)
