7ba95b58a5f5167ab9e390018afe9d4b4d43d4bb
Vlad Ivanov <vivanov@metaprime.ru>
2020-04-02 00:27:16 +0300

    fifo

--- module.py
+++ module.py
@@ -1,31 +1,9 @@
-def parse(string):
-    rules = string.split(' ')
-    result_dict = {}
-    for rule in rules:
-        if rule != '':
-            [rule_for, rule_val] = rule.split('=')
-            result_dict[int(rule_for)] = float(rule_val)
-    return result_dict
-
-
-def stringify(rules):
-    result_string = ''
-    for rule in rules:
-        result_string += str(rule) + '=' + str(rules[rule]) + ' '
-    return result_string
-
+import os
 
 class PWM:
-    def __init__(self, pin, path='/dev/pi-blaster'):
+    def __init__(self, pin):
         self.pin = pin
-        self.path = path
 
     def set(self, value):
-        i = open(self.path, 'r')
-        now = i.read()
-        i.close()
-        parsed = parse(now)
-        parsed[self.pin] = value
-        o = open(self.path, 'w')
-        o.write(stringify(parsed))
-        o.close()
+        cmd = 'echo \"' + str(self.pin) + '=' + str(value) + '\" > /dev/pi-blaster'
+        os.system(cmd)
