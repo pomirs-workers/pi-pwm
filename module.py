d801e23a2e3409067beea8b5981c42e2d953392d
Vlad Ivanov <vivanov@metaprime.ru>
2020-04-01 21:42:10 +0300

    upd

--- module.py
+++ module.py
@@ -1,14 +1,31 @@
-import os
+def parse(string):
+    rules = string.split(' ')
+    result_dict = {}
+    for rule in rules:
+        if rule != '':
+            [rule_for, rule_val] = rule.split('=')
+            result_dict[int(rule_for)] = float(rule_val)
+    return result_dict
+
+
+def stringify(rules):
+    result_string = ''
+    for rule in rules:
+        result_string += str(rule) + '=' + str(rules[rule]) + ' '
+    return result_string
 
 
 class PWM:
-    def __init__(self):
-        pass
+    def __init__(self, pin, path='/dev/pi-blaster'):
+        self.pin = pin
+        self.path = path
 
-    @staticmethod
-    def set(values):
-        cmd = 'echo \"'
-        for pin in values:
-            cmd += str(pin) + '=' + str(values[pin]) + ';'
-        cmd += '\" > /dev/pi-blaster'
-        os.system(cmd)
+    def set(self, value):
+        i = open(self.path, 'r')
+        now = i.read()
+        i.close()
+        parsed = parse(now)
+        parsed[self.pin] = value
+        o = open(self.path, 'w')
+        o.write(stringify(parsed))
+        o.close()
